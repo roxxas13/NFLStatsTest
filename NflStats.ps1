e22765f98626e160b1b5184f9dfc002aebc91dc2
roxxas13 <112147405+roxxas13@users.noreply.github.com>
2022-08-25 23:54:41 -0400

    Add files via upload

---
+++ NflStats.ps1
@@ -0,0 +1,3 @@
+﻿$stats = Invoke-RestMethod -uri https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2021/types/2/teams/11/statistics/0
+
+$stats | %{$_.splits} |%{$_.categories -match "defensive"}| %{$_.stats} | ?{$_.name -match "yardsAllowed"}
