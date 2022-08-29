9c345f003c3213502609fedca71435e56d315e58
roxxas13 <112147405+roxxas13@users.noreply.github.com>
2022-08-29 01:25:10 -0400

    Add files via upload

--- NflStats.ps1
+++ NflStats.ps1
@@ -1,3 +1,37 @@
-﻿$stats = Invoke-RestMethod -uri https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2021/types/2/teams/11/statistics/0
+﻿#Categories
+$CAT_MISC = 10
+$CAT_RUSH = 2
 
-$stats | %{$_.splits} |%{$_.categories -match "defensive"}| %{$_.stats} | ?{$_.name -match "yardsAllowed"}
+
+$STAT_POSS = 9
+$STAT_YPC = 28
+
+
+##Types in the uri
+$Preseason = 1
+$RegSeason = 2
+$PostSeason = 3
+$OffSeason = 4
+
+
+
+$teams= Invoke-RestMethod -uri https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/teams?limit=100 | foreach-object { $_.items } | foreach-object { invoke-restmethod $_.'$ref' } | ? { $_.isActive } 
+
+#This line grabs the stats for every team
+$teams | % { Add-Member -InputObject $_ -MemberType NoteProperty -Name 'stats' -Value (Invoke-RestMethod -uri https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/seasons/2021/types/$RegSeason/teams/$($_.id)/statistics/0) }
+
+
+#adds the teams schedule
+$teams | % { Add-Member -InputObject $_ -MemberType NoteProperty -Name 'schedule' -Value (Invoke-RestMethod -uri https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/$($_.id)/schedule) }
+
+remove-item c:\Scripts\espn.csv -erroraction silentlycontinue
+$teams | foreach-object {
+    $curTeam = $_
+    $teamName = $curTeam.name
+    $possTimeSeconds = $curTeam.stats.splits.categories[$CAT_MISC].stats[$STAT_POSS].displayValue
+    $possRank = $curTeam.stats.splits.categories[$CAT_MISC].stats[$STAT_POSS].rankDisplayValue
+    $YPC= $curTeam.stats.splits.categories[$CAT_RUSH].stats[$STAT_YPC].displayValue
+    $YPCRANK = $curTeam.stats.splits.categories[$CAT_RUSH].stats[$STAT_YPC].rankDisplayValue
+    "$teamName,$possTimeSeconds,$possRank,$YPC,$YPCRANK"
+} | out-file -append c:\Scripts\espn.csv
+
